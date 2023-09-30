3d9ed3b56c9f260d326f156216c90b5bdd424710
dengty1998 <dengty1998@outlook.com>
2023-09-30 23:25:56 +0200

    Formatted codes

--- app.py
+++ app.py
@@ -5,20 +5,26 @@ app = Flask(__name__)
 app.secret_key = 'secret'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @socketio.on('join', namespace='/chat')
 def join(message):
     room = message['room']
     join_room(room)
-    send({'msg': message['username'] + " join the " + message['room'] + " channel."}, room=room)
+    send({'msg': message['username'] + " join the " +
+         message['room'] + " channel."}, room=room)
+
 
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = message['room']
-    send({'msg': message['username'] +"["+ message['room'] + "]: " + message['msg']}, room=room)
+    send({'msg': message['username'] + "[" +
+         message['room'] + "]: " + message['msg']}, room=room)
+
 
 @socketio.on('left', namespace='/chat')
 def left(message):
@@ -26,7 +32,8 @@ def left(message):
     leave_room(room)
     send({'msg': message['username'] + " leave the channel."}, room=room)
 
+
 # change network profile from public to private then replace host argument with server's ip
 if __name__ == '__main__':
-    socketio.run(app,# host = '130.229.130.154', port = 5000, 
+    socketio.run(app,  # host = '130.229.130.154', port = 5000,
                  debug=True, allow_unsafe_werkzeug=True)
