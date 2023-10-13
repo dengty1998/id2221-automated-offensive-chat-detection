24055e737b0af95fd46cf652e9f321ced685448b
dengty1998 <dengty1998@outlook.com>
2023-10-13 16:16:25 +0200

    Added a basic login function

--- app.py
+++ app.py
@@ -1,17 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room, send
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.secret_key = 'secret'
 socketio = SocketIO(app, cors_allowed_origins="*")
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+users = {'dty': {'password': '123456'}, 'amy': {'password': '654321'}}
+
+
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User()
+    user.id = user_id
+    return user
 
 
 @app.route('/')
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users and users[username]['password'] == password:
+            user = User()
+            user.id = username
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            return 'Login failed. Please check your credentials.'
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
     return render_template('index.html')
 
 
 @socketio.on('join', namespace='/chat')
+@login_required
 def join(message):
     room = message['room']
     join_room(room)
@@ -20,6 +74,7 @@ def join(message):
 
 
 @socketio.on('text', namespace='/chat')
+@login_required
 def text(message):
     room = message['room']
     send({'msg': message['username'] + "[" +
@@ -27,6 +82,7 @@ def text(message):
 
 
 @socketio.on('left', namespace='/chat')
+@login_required
 def left(message):
     room = message['room']
     leave_room(room)
